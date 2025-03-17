ae3b56fc3efae4c7241c9bb2d74f9c31174fea62
germa <germa@GERMANJR-10>
2025-03-17 01:43:36 -0400

    Add project files.

---
+++ Aprovacion de voto/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Aprovacion_de_voto
+{
+    class program
+    {
+        static void Main (string[] args)
+        {
+            Console.WriteLine("Estimado/a ingrese su nombre y apellido:");
+            Console.ReadLine();
+            int edad;
+            Console.WriteLine("Ingrese su edad");
+            edad = int.Parse(Console.ReadLine());
+            if (edad >= 18)
+            {
+                Console.WriteLine("Usted puede votar");
+
+
+            }
+
+            else
+            {
+                Console.WriteLine("Usted no puede votar");
+            }
+            
+                 
+
+
+
+        }
+    }
+}
